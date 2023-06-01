56050e9a30349fb6c7c83566e1acc4fe53cb7e7d
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-06-01 16:17:31 +0300

    [Lesson38]: create input, textarea and username components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //step 3 Button
 import Button, { ButtonTypes } from './components/Button';
 //step 3 Title
 import Title from './components/Title';
 //step 3 Tabs
-import Tabs, { TabsTypes }from './components/Tabs';
+import Tabs, { TabsTypes } from './components/Tabs';
 //step 3 Tabslist
 import Tabslist from './components/Tabslist';
 //step 3 Card
 import Card from './components/Card';
 import { CardTypes } from './components/Card';
+//step 3 Input
+import Input from './components/Input';
+import Usermane from './components/Username';
 
 
 
@@ -21,6 +24,13 @@ import { CardTypes } from './components/Card';
 // }
 
 const App = () => {
+
+  // создаем для работы input
+  const [inputValue, setInputValue] = useState ('');
+  const onChange = (value: string) => {
+    setInputValue(value)
+  }
+
   return <div>
 
     {/* //step 4 Button */}
@@ -37,40 +47,48 @@ const App = () => {
 
     {/* //step 4 Card */}
     <Card
-        type={CardTypes.Large}
-        date='April 20, 2021'
-        title='Astronauts prep for new solar arrays on nearly seven-hour spacewalk'
-        text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.'
-        image='https://gamerwall.pro/uploads/posts/2022-02/1645708691_1-gamerwall-pro-p-astronavt-v-kosmose-krasivie-oboi-1.jpg'
-        id={0}
-        lesson_num={0}
-        author={0}
-      />
+      type={CardTypes.Large}
+      date='April 20, 2021'
+      title='Astronauts prep for new solar arrays on nearly seven-hour spacewalk'
+      text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.'
+      image='https://gamerwall.pro/uploads/posts/2022-02/1645708691_1-gamerwall-pro-p-astronavt-v-kosmose-krasivie-oboi-1.jpg'
+      id={0}
+      lesson_num={0}
+      author={0}
+    />
 
-      <Card
-        type={CardTypes.Medium}
-        date='April 20, 2021'
-        title='Astronauts prep for new solar arrays on nearly seven-hour spacewalk'
-        text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.'
-        image='https://gamerwall.pro/uploads/posts/2022-02/1645708691_1-gamerwall-pro-p-astronavt-v-kosmose-krasivie-oboi-1.jpg'
-        id={0}
-        lesson_num={0}
-        author={0}
-      />
+    <Card
+      type={CardTypes.Medium}
+      date='April 20, 2021'
+      title='Astronauts prep for new solar arrays on nearly seven-hour spacewalk'
+      text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.'
+      image='https://gamerwall.pro/uploads/posts/2022-02/1645708691_1-gamerwall-pro-p-astronavt-v-kosmose-krasivie-oboi-1.jpg'
+      id={0}
+      lesson_num={0}
+      author={0}
+    />
 
-      <Card
-        type={CardTypes.Small}
-        date='April 20, 2021'
-        title='Astronauts prep for new solar arrays on nearly seven-hour spacewalk'
-        text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.'
-        image='https://gamerwall.pro/uploads/posts/2022-02/1645708691_1-gamerwall-pro-p-astronavt-v-kosmose-krasivie-oboi-1.jpg'
-        id={0}
-        lesson_num={0}
-        author={0}
-      />
+    <Card
+      type={CardTypes.Small}
+      date='April 20, 2021'
+      title='Astronauts prep for new solar arrays on nearly seven-hour spacewalk'
+      text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.'
+      image='https://gamerwall.pro/uploads/posts/2022-02/1645708691_1-gamerwall-pro-p-astronavt-v-kosmose-krasivie-oboi-1.jpg'
+      id={0}
+      lesson_num={0}
+      author={0}
+    />
 
+    <Input
+      isTextarea
+      title={'Test Input'}
+      placeholder={'Hello World'}
+      onChange={onChange}
+      value={inputValue}
+    // errorText={'Error'}
+    />
 
-
+    <Usermane username={'Katsiaryna'} />
 
   </div>
 }
